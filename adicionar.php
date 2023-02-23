e2ffc42c2695c4ae06574f9db4f499d3b4998cbc
MarlonnLima <goncalvesmarlon072@gmail.com>
2023-02-22 23:23:35 -0300

    correção no location

--- adicionar.php
+++ adicionar.php
@@ -40,7 +40,7 @@ if (isset($_SESSION)):
     include_once 'includes/footer.php';
 
     else:
-        header("Location: admin.index.php");
+        header("Location: index.php");
     endif; // da session
 endif; // do isset
 ?>
